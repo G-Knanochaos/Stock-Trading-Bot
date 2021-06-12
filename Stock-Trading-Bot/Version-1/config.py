f1041c6520b64c32121d5845f52298a5ba85681c
SirCraftinator <73557277+SirCraftinator@users.noreply.github.com>
2021-06-11 17:12:57 -0700

    Delete Stock-Trading-Bot directory

--- Stock-Trading-Bot/Version-1/config.py
+++
@@ -1,4 +0,0 @@
-API_KEY = 'PKO58Y0VACIOMI4CNUWJ'
-API_SECRET = 'fDXcPbqAwftUkLnChJwQrr3iHaAVARNH1zt1LSO9'
-API_BASE_URL = 'https://paper-api.alpaca.markets'
-
--- Stock-Trading-Bot/config.py
+++
@@ -1,4 +0,0 @@
-API_KEY = '(insert key here)'
-API_SECRET = '(insert secret key here)'
-API_BASE_URL = 'https://paper-api.alpaca.markets'
-
