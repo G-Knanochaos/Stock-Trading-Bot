338f2676906f334bd536dd99c8000ce77f6d8b70
Gavin-kai Vida <73557277+G-Knanochaos@users.noreply.github.com>
2021-12-27 21:21:39 -0800

    Update config.py

--- Version-1/config.py
+++ Version-1/config.py
@@ -1,4 +1,3 @@
-API_KEY = 'PK1BWB9Z46VDDBBO9R78'
-API_SECRET = 'JMbLILnnhndkVXHuQRlUdAhM1pdAG2gBR5rrcqY8'
-API_BASE_URL = 'https://paper-api.alpaca.markets'
-
+API_KEY = 'Insert key here'
+API_SECRET = 'Insert secret here'
+API_BASE_URL = 'https://paper-api.alpaca.markets' #there URl is for paper trading
