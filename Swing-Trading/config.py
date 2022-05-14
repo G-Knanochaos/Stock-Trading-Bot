e629debf93dc145f566eaeb91394e1a20a346915
Gavin-kai Vida <73557277+G-Knanochaos@users.noreply.github.com>
2022-05-14 14:10:38 -0700

    Update config.py

--- Swing-Trading/config.py
+++ Swing-Trading/config.py
@@ -1,6 +1,6 @@
 import alpaca_trade_api as tradeapi
-API_KEY = 'PK0XYXTEZPTUPH1RXAIX'
-API_SECRET = 'VMwsxO88C4yFn5PwGK2fO7LXPucRaMumJPqSkB16'
+API_KEY = ''
+API_SECRET = ''
 API_BASE_URL = 'https://paper-api.alpaca.markets'
 api = tradeapi.REST(API_KEY, API_SECRET, API_BASE_URL)
 
